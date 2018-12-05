489fa4c73a388af64e6e2104ccbf91209eff30d8
Crawler <33508834+changshengliu312@users.noreply.github.com>
2018-12-05 10:15:24 +0800

    Delete testForm.py

--- Mul-DownLoad_Video/Form/testForm.py
+++
@@ -1,6 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class testForm(QtWidgets.QDialog):
-    def PushButtonClicked(self):
-            box = QtWidgets.QMessageBox()
-            box.warning(self,"提示","这是一个按钮事件")
